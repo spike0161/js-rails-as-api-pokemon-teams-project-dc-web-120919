2beb1fe6169884695019b35f92836fa8fb84741d
github username <github email address>
2020-02-04 16:52:22 -0500

    Cards are working

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,28 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  fetchAllTrainers()
+})
+
+function fetchAllTrainers() {
+  fetch(TRAINERS_URL)
+  .then(resp => resp.json())
+  .then(trainers => trainers.forEach(trainer => {
+    buildTrainerCard(trainer)
+  }))
+}
+
+
+function buildTrainerCard(trainer) {
+  let mainDiv = document.getElementById('trainer-collection')
+  let trainerDiv = document.createElement('div')
+  trainerDiv.className = 'card'
+  let trainerName = document.createElement('p')
+  trainerName.innerText = trainer.name
+
+  trainerDiv.appendChild(trainerName)
+  mainDiv.appendChild(trainerDiv)
+
+}
